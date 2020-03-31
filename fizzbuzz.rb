3cc5e412b3e9a33ac8da23a16d602b13443b42b3
Samantha Balgobin <sbalgobin94@gmail.com>
2020-03-31 22:54:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,13 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
+
+  if int % 5 == 0
+    "Buzz"
+  end
+
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
