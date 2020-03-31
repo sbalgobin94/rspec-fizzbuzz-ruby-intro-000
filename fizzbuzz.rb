09c5527e6a8c10210f66532e1283d8e8ffd07e05
Samantha Balgobin <sbalgobin94@gmail.com>
2020-03-31 22:56:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   end
 
-  if int % 5 == 0
+elsif int % 5 == 0
     "Buzz"
   end
 
-  if int % 3 == 0 && int % 5 == 0
+elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
 end# Don't forget! This file needs to be 'required' in its spec file
